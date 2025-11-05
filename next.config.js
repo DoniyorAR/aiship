0c854b1c3890d16e4738448a125df62793a37649
DoniyorAR <84126471+DoniyorAR@users.noreply.github.com>
2025-11-05 21:14:57 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async redirects() {
-    return [
-      { source: '/', destination: '/overall', permanent: false },
-    ];
+    return [{ source: '/', destination: '/overall', permanent: false }];
   },
 };
 module.exports = nextConfig;
