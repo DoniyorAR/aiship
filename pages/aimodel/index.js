5132ab100c30ca3c1a496a8915b560af0a497562
DoniyorAR <84126471+DoniyorAR@users.noreply.github.com>
2025-11-05 21:05:07 +0900

    Delete pages directory

--- pages/aimodel/index.js
+++
@@ -1,8 +0,0 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-
-export default function AiModelIndex() {
-  const router = useRouter();
-  useEffect(() => { router.replace('/aimodel/visualization') }, [router]);
-  return null;
-}
--- pages/datavalidation/index.js
+++
@@ -1,7 +0,0 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-export default function DataValidationIndex() {
-  const router = useRouter();
-  useEffect(() => { router.replace('/datavalidation/datasets') }, [router]);
-  return null;
-}
